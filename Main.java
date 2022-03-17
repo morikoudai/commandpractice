f130eda189d3c962f3f15a6ce569d97816ee51e4
Koudai Mori <k.mori.sys21@morijyobi.ac.jp>
2022-03-17 11:57:09 +0900

    Main.javaの変更

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        double BMI, height, weight;
+        double BMI, height, weight, i;
         height=1.74;
         weight=60;
         BMI = weight / height / height;
+        for(i=0;i<2;i++) {
         System.out.println(BMI);
+        }
     }
 }
