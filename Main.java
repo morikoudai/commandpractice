e8686aa43289abffe7de8348a08a6e79e1f6e55c
Koudai Mori <k.mori.sys21@morijyobi.ac.jp>
2022-03-17 11:41:44 +0900

    add .gitignore

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double BMI, height, weight;
+        height=1.74;
+        weight=60;
+        BMI = weight / height / height;
+        System.out.println(BMI);
+    }
+}
